47368b1414aaa4603d82dd6eb925db2f0079a69f
Ravager2019 <142700858+Ravager2019@users.noreply.github.com>
2025-06-23 13:07:54 -0700

    Add shared header and footer

---
+++ layout.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const header = `\
+    <header>
+      <nav>
+        <a href="index.html">Home</a>
+        <a href="about.html">About</a>
+        <a href="experience.html">Experience</a>
+        <a href="skills.html">Skills</a>
+        <a href="contact.html">Contact</a>
+        <a href="projects.html">Projects</a>
+      </nav>
+    </header>`;
+
+  const footer = `\
+    <footer>
+      <p>&copy; 2024 Tanner Hardman</p>
+    </footer>`;
+
+  document.body.insertAdjacentHTML('afterbegin', header);
+  document.body.insertAdjacentHTML('beforeend', footer);
+});
